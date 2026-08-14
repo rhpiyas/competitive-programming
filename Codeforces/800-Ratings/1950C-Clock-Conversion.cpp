33cf51b1cebd999933683d876fe0419424bc5ac0
Rakib Hasan Piyas <rakibhasanpiyas97@gmail.com>
2026-08-14 21:46:48 +0600

    Coded the structure 1950C-Clock Conversion

---
+++ Codeforces/800-Ratings/1950C-Clock-Conversion.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while(t--)
+    {
+        string s;
+        cin >> s;
+
+        int hour = (s[0]-'0')*10 + (s[1]-'0');
+        
+        int min = (s[3]-'0')*10 + (s[4]-'0');
+
+        
+    }
+
+
+    return 0;
+}
