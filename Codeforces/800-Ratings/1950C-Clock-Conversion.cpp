d2a4bfde44122cf75ade9b711e3093bc6de4e174
Rakib Hasan Piyas <rakibhasanpiyas97@gmail.com>
2026-08-15 19:17:09 +0600

    Solved 1950C - Clock conversion problem in codeforces 800 ratings

--- Codeforces/800-Ratings/1950C-Clock-Conversion.cpp
+++ Codeforces/800-Ratings/1950C-Clock-Conversion.cpp
@@ -15,7 +15,44 @@ int main()
         
         int min = (s[3]-'0')*10 + (s[4]-'0');
 
-        
+        string count = "AM";
+
+        if(hour >= 12)
+        {
+            count = "PM";
+        }
+
+        if(hour > 12)
+        {
+            hour = hour - 12;
+        }
+
+        if(hour == 0)
+        {
+            hour = 12;
+        }
+
+        if(hour < 10)
+        {
+            cout << 0 << hour;
+        }
+        else
+        {
+            cout << hour;
+        }
+
+        cout << ":";
+
+        if(min<10)
+        {
+            cout << 0 << min;
+        }
+        else
+        {
+            cout << min;
+        }
+
+        cout << " " << count << endl;
     }
 
 
