665a5bb0943ebe8200ff9690632ff953c2714553
Rakib Hasan Piyas <rakibhasanpiyas97@gmail.com>
2026-09-23 21:05:06 +0600

    Solved 1950B-Upscalling problem in codeforces 800 ratings

---
+++ Codeforces/800-Ratings/1950B-Upscalling.cpp
@@ -0,0 +1,78 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while(t--)
+    {
+        int n;
+        cin >> n;
+
+        for(int i=0; i<n; i++)
+        {
+            if(i%2==0)
+            {
+                for(int j=0; j<n; j++)
+                {
+                    if(j%2==0)
+                    {
+                        cout << "##";
+                    }
+                    else
+                    {
+                        cout << "..";
+                    }
+                }
+                cout << endl;
+
+                for(int j=0; j<n; j++)
+                {
+                    if(j%2==0)
+                    {
+                        cout << "##";
+                    }
+                    else
+                    {
+                        cout << "..";
+                    }
+                }
+                cout << endl;
+            }
+            else
+            {
+                for(int j=0; j<n; j++)
+                {
+                    if(j%2==0)
+                    {
+                        cout << "..";
+                    }
+                    else
+                    {
+                        cout << "##";
+                    }
+                }
+                cout << endl;
+
+                for(int j=0; j<n; j++)
+                {
+                    if(j%2==0)
+                    {
+                        cout << "..";
+                    }
+                    else
+                    {
+                        cout << "##";
+                    }
+                }
+                cout << endl;
+            }
+        }
+    }
+
+
+
+    return 0;
+}
